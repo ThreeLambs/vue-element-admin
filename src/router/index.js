87d11901daf8a7e7ca7344f5049f82c27980f767
qun <qun0297@hotmail.com>
2021-12-17 16:24:17 +0800

    lint

--- src/router/index.js
+++ src/router/index.js
@@ -7,30 +7,45 @@ Vue.use(Router)
 import Layout from '@/layout'
 
 /**
- * Note: sub-menu only appear when route children.length >= 1
- * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
- *
- * hidden: true                   if set true, item will not show in the sidebar(default is false)
- * alwaysShow: true               if set true, will always show the root menu
- *                                if not set alwaysShow, when item has more than one children route,
- *                                it will becomes nested mode, otherwise not show the root menu
- * redirect: noRedirect           if set noRedirect will no redirect in the breadcrumb
- * name:'router-name'             the name is used by <keep-alive> (must set!!!)
- * meta : {
-    roles: ['admin','editor']    control the page roles (you can set multiple roles)
-    title: 'title'               the name show in sidebar and breadcrumb (recommend set)
-    icon: 'svg-name'/'el-icon-x' the icon show in the sidebar
-    noCache: true                if set true, the page will no be cached(default is false)
-    affix: true                  if set true, the tag will affix in the tags-view
-    breadcrumb: false            if set false, the item will hidden in breadcrumb(default is true)
-    activeMenu: '/example/list'  if set path, the sidebar will highlight the path you set
-  }
+  * 侧边栏和路由是绑定在一起的，所以你只有在 @/router/index.js 下面配置对应的路由，侧边栏就能动态的生成了。
+ * 大大减轻了手动重复编辑侧边栏的工作量。当然这样就需要在配置路由的时候遵循一些约定的规则
+
+路由分类： constantRoutes 和 asyncRoutes
+constantRoutes： 公共路由，如登录页、404、等通用页面
+asyncRoutes： 代表那些需求动态判断权限并通过 addRoutes 动态添加的页面
+
+路由配置选项：
+ hidden: true // (默认 false) 隐藏菜单项，如401，login等页面，或者如一些编辑页面/edit/1
+
+ //当设置 noRedirect 的时候该路由在面包屑导航中不可被点击
+ redirect: 'noRedirect'
+
+ // 当你一个路由下面的 children 声明的路由大于1个时，自动会变成嵌套的模式
+ // 只有一个时，会将那个子路由当做根路由显示在侧边栏
+ // 若你想不管路由下面的 children 声明的个数都显示你的根路由。你可以设置 alwaysShow: true，这样它就会忽略之前定义的规则，一直显示根路由
+ alwaysShow: true  根路由总是显示
+
+ name: 'router-name' // 设定路由的名字，一定要填写不然使用<keep-alive>时会出现各种问题
+ meta: {
+   roles: ['admin', 'editor'] // 角色授权
+   pollcy: 'User.Edit' //策略授权
+   title: 'title' // 设置该路由在侧边栏和面包屑中展示的名字
+   icon: 'svg-name' // 设置该路由的图标，支持 svg-class，也支持 el-icon-x element-ui 的 icon
+   noCache: true // 如果设置为true，则不会被 <keep-alive> 缓存(默认 false)
+   breadcrumb: false //  如果设置为false，则不会在breadcrumb面包屑中显示(默认 true)
+   affix: true // 如果设置为true，它则会固定在tags-view中(默认 false)
+
+   // 当路由设置了该属性，则会高亮相对应的侧边栏。
+   // 这在某些场景非常有用，比如：一个文章的列表页路由为：/article/list
+   // 点击文章进入文章详情页，这时候路由为/article/1，但你想在侧边栏高亮文章列表的路由，就可以进行如下设置
+   activeMenu: '/article/list'
+ }
+ 其它的配置和 vue-router 官方并没有区别，自行查看文档
  */
 
 /**
  * constantRoutes
- * a base page that does not have permission requirements
- * all roles can be accessed
+ * 公共路由，无访问权限要求
  */
 export const constantRoutes = [
   {
@@ -93,18 +108,21 @@ export const asyncRoutes = [
       title: 'Error Pages',
       icon: '404'
     },
+    hidden: true,
     children: [
       {
         path: '401',
         component: () => import('@/views/error-page/401'),
         name: 'Page401',
-        meta: { title: '401', noCache: true }
+        meta: { title: '401', noCache: true },
+        hidden: true
       },
       {
         path: '404',
         component: () => import('@/views/error-page/404'),
         name: 'Page404',
-        meta: { title: '404', noCache: true }
+        meta: { title: '404', noCache: true },
+        hidden: true
       }
     ]
   },
@@ -113,7 +131,7 @@ export const asyncRoutes = [
 ]
 
 const createRouter = () => new Router({
-  // mode: 'history', // require service support
+  mode: 'history', // require service support
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
 })
